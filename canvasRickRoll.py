4bc9b822038fde54e7134adaf00a53859454bb2c
RadioLogic <ncottrellweb@gmail.com>
2022-04-12 08:55:25 -0400

    Initial prototype for main program

--- canvasRickRoll.py
+++ canvasRickRoll.py
@@ -1,1 +1,8 @@
 from mitmproxy import http
+
+
+def response(flow: http.HTTPFlow) -> None:
+    if "fit.instructure.com" in flow.request.pretty_host:
+        print(flow.request.content)
+        if "<button type=\"submit\" class=\"btn btn-primary\" id=\"submit_file_button\">Submit Assignment</button>" in flow.request.content:
+            print("The button is here!")
