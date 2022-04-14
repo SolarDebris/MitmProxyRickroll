820b3a3fc1bac72a8700ce4c4fb3b98df4c4e3fb
RadioLogic <ncottrellweb@gmail.com>
2022-04-14 08:46:13 -0400

    Made form onsubmit go to rickroll

--- canvasRickRoll.py
+++ canvasRickRoll.py
@@ -3,6 +3,6 @@ from mitmproxy import http
 
 def response(flow: http.HTTPFlow) -> None:
     if "fit.instructure.com" in flow.request.pretty_host:
-        print(flow.request.content)
-        if "<button type=\"submit\" class=\"btn btn-primary\" id=\"submit_file_button\">Submit Assignment</button>" in flow.request.content:
-            print("The button is here!")
+        if b"<button type=\"submit\" class=\"btn btn-primary\" id=\"submit_file_button\">Submit Assignment</button>" in flow.response.content and b"<form id=\"submit_online_upload_form\"" in flow.response.content:
+            flow.response.content = flow.response.content.replace(
+                b"<form id=\"submit_online_upload_form\"", b"<form id=\"submit_online_upload_form\" onsubmit=\"window.location.href=\'https://youtu.be/dQw4w9WgXcQ\'\"")
